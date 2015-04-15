f3b9db2193334020f8107569990d31e30be92999
Cosmin Banu <cosminb@gmail.com>
2015-04-15 22:46:18 +0300

    Added more packages to install. Added basic setup for ZSH.

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-sudo apt-get install mc
+# @todo: check if it is run using sudo and suggest to run as regular user
+
 sudo apt-get install git
-sudo apt-get install vim
+sudo apt-get install mc
 sudo apt-get install openssh-server
+sudo apt-get install vim
+sudo apt-get install screen
+sudo apt-get install tmux
+sudo apt-get install zsh
+
+# create a default empty zshrc file, to avoid first usage prompts
+if [ ! -f ~/.zshrc ]; then
+	touch ~/.zshrc
+fi
+
+sudo chsh -s `which zsh` ${USER}
+
