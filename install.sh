50fb37dc8975781c50f564e546b6354d7e1ac677
Cosmin Banu <cosminb@gmail.com>
2015-04-16 18:22:57 +0300

    Fixed file-exists tests. Pulled in ZSH prompt theme 'pure'.

--- install.sh
+++ install.sh
@@ -13,27 +13,38 @@ sudo apt-get install tmux
 sudo apt-get install zsh
 
 # create a default empty zshrc file, to avoid first usage prompts
-if [ ! -f ~/.zshrc ]; then
+if [ ! -e "${HOME}/.zshrc" ]; then
 	touch ~/.zshrc
 	echo "Created '~/.zshrc' placeholder."
 fi
 
 ZSH_SHELL=`which zsh`
-if [ ${ZSH_SHELL} != ${SHELL} ]; then
+if [ "${ZSH_SHELL}" != "${SHELL}" ]; then
 	echo "Switching user shell to ZSH."
 	chsh -s ${ZSH_SHELL} ${USER}
 fi
 
 # create symlinks
-if [ ! -f ~/.zshrc_local ]; then
+if [ ! -e "${HOME}/.zshrc_local" ]; then
 	echo "Creating '~/.zshrc_local' symlink."
 	ln -s dotfiles/zsh/.zshrc_local ~/.zshrc_local
 fi
 
 # source local configurations
-if [ -f ~/.zshrc ]; then
+if [ -e "${HOME}/.zshrc" ]; then
         if ! grep -F ". ~/.zshrc_local" ~/.zshrc 2>/dev/null ; then
                 echo "Sourcing local configuration into '.zshrc'."
                 cat ~/dotfiles/zsh/.zshrc_source >> ~/.zshrc
         fi
 fi
+
+# setup ZSH prompt
+if [ ! -e "zsh/prompt/pure" ]; then
+	mkdir -p zsh/prompt/pure
+	cd zsh/prompt/pure
+	git init
+	git remote add origin https://github.com/sindresorhus/pure.git
+	git config core.sparseCheckout true
+	echo "pure.zsh" > .git/info/sparse-checkout
+fi
+git pull origin master
