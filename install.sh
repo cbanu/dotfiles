de2da3a5fc2962deaacc703697dd06126de0db10
Cosmin Banu <cosminb@gmail.com>
2015-04-28 12:44:01 +0300

    Added vundle plugin manager for VIM. Refactored existing code into methods for creating placeholders and symlinks. Retabbed 'install.sh'.

--- install.sh
+++ install.sh
@@ -4,88 +4,112 @@ DOTFILES=`basename ${PWD}`
 DOTFILES_DIR=${PWD}
 # @todo: check if it is run using sudo and suggest to run as regular user
 
-echo "Installing packages..."
+# creates empty placeholder file (only if the file doesn't exist)
+createPlaceholder() {
+    file=$1
+    if [ ! -e "${file}" ] ; then
+        touch "${file}"
+        echo "Created '${file}' placeholder."
+    else
+        echo "File '${file}' already exists."
+    fi
+}
 
+# creates a symlink to the specified target (only if symlink file doesn't exist)
+createSymlink() {
+    link=$1
+    target=$2
+    if [ -h "${link}" ] ; then
+        linkTarget=`readlink ${link}`
+        if [ "${linkTarget}" != "${target}" ] ; then
+            echo "ERROR: Symlink '${link}' already exists, but points to wrong target '${linkTarget}'." 1>&2
+            exit 1
+        else
+            echo "Symlink '${link}' already exists."
+        fi
+    elif [ -e "${link}" ] ; then
+        echo "ERROR: File '${link}' already exists and it's not a symlink." 1>&2
+        exit 1
+    else
+        ln -s "${target}" "${link}"
+        echo "Created '${link}' symlink."
+    fi
+}
+
+# source local configurations
+sourceLocalConfig() {
+    configFile=$1
+    match=$2
+    sourceFile=$3
+    if [ -e "${configFile}" ] ; then
+        if ! grep -F "${match}" "${configFile}" 2>/dev/null ; then
+            echo "Sourcing local configuration into '${configFile}'."
+            cat "${sourceFile}" >> "${configFile}"
+        fi
+    fi
+}
+
+echo "Installing packages..."
 if command -v apt-get 2>/dev/null ; then
-	sudo apt-get install git-core mc openssh-server vim screen tmux zsh
+    sudo apt-get install git-core mc openssh-server vim screen tmux zsh
 elif command -v pacman 2>/dev/null ; then
-	sudo pacman -S --noconfirm git mc openssh vim screen tmux zsh
+    sudo pacman -S --noconfirm git mc openssh vim screen tmux zsh
 else
-	echo "No suitable package manager found."
-	exit 1
+    echo "No suitable package manager found."
+    exit 1
 fi
 
 # change shell to ZSH
 ZSH_SHELL=`which zsh`
 if [ "${ZSH_SHELL}" != "${SHELL}" ]; then
-	echo "Switching user shell to ZSH."
-	chsh -s ${ZSH_SHELL} ${USER}
+    echo "Switching user shell to ZSH."
+    chsh -s ${ZSH_SHELL} ${USER}
 fi
 
 # create a default empty zshrc file, to avoid first usage prompts
-if [ ! -e "${HOME}/.zshrc" ]; then
-	touch ~/.zshrc
-	echo "Created '~/.zshrc' placeholder."
-fi
+createPlaceholder ${HOME}/.zshrc
 
 # create a default empty vimrc file
-if [ ! -e "${HOME}/.vimrc" ]; then
-	touch ~/.vimrc
-	echo "Created '~/.vimrc' placeholder."
-fi
+createPlaceholder ${HOME}/.vimrc
 
 # create symlinks
-if [ ! -e "${HOME}/.zshrc_local" ]; then
-	echo "Creating '~/.zshrc_local' symlink."
-	ln -s ${DOTFILES}/zsh/.zshrc_local ~/.zshrc_local
-fi
-if [ ! -e "${HOME}/.vimrc_local" ]; then
-	echo "Creating '~/.vimrc_local' symlink."
-	ln -s ${DOTFILES}/vim/.vimrc_local ~/.vimrc_local
-fi
+createSymlink ${HOME}/.zshrc_local ${DOTFILES}/zsh/.zshrc_local
+createSymlink ${HOME}/.vimrc_local ${DOTFILES}/vim/.vimrc_local
+createSymlink ${HOME}/.vimrc_vundle ${DOTFILES}/vim/.vimrc_vundle
 
 # source local configurations
-if [ -e "${HOME}/.zshrc" ]; then
-        if ! grep -F ".zshrc_local" ~/.zshrc 2>/dev/null ; then
-                echo "Sourcing local configuration into '.zshrc'."
-                cat ~/${DOTFILES}/zsh/.zshrc_source >> ~/.zshrc
-        fi
-fi
-if [ -e "${HOME}/.vimrc" ]; then
-        if ! grep -F ".vimrc_local" ~/.vimrc 2>/dev/null ; then
-                echo "Sourcing local configuration into '.vimrc'."
-                cat ~/${DOTFILES}/vim/.vimrc_source >> ~/.vimrc
-        fi
-fi
+sourceLocalConfig ${HOME}/.zshrc ".zshrc_local" ~/${DOTFILES}/zsh/.zshrc_source
+sourceLocalConfig ${HOME}/.vimrc ".vimrc_local" ~/${DOTFILES}/vim/.vimrc_source
 
 # setup ZSH prompt
 # @todo: how to handle authentication without passphrase prompt?
 cd ${DOTFILES_DIR}
 if [ ! -e "zsh/prompt/pure" ]; then
-	mkdir -p zsh/prompt/pure
-	cd zsh/prompt/pure
-	git init
-	git remote add origin https://github.com/sindresorhus/pure.git
-	git config core.sparseCheckout true
-	echo "pure.zsh" > .git/info/sparse-checkout
-	git pull origin master
-	patch pure.zsh ../../pure.zsh.patch
+    mkdir -p zsh/prompt/pure
+    cd zsh/prompt/pure
+    git init
+    git remote add origin https://github.com/sindresorhus/pure.git
+    git config core.sparseCheckout true
+    echo "pure.zsh" > .git/info/sparse-checkout
+    git pull origin master
+    patch pure.zsh ../../pure.zsh.patch
 else
-	cd zsh/prompt/pure
-	git checkout pure.zsh
-	git pull origin master
-	patch pure.zsh ../../pure.zsh.patch
+    cd zsh/prompt/pure
+    git checkout pure.zsh
+    git pull origin master
+    patch pure.zsh ../../pure.zsh.patch
 fi
 
 # setup vundle package manager for VIM
-cd ${DOTFILES_DIR}
-if [ ! -e "vim/vundle" ]; then
-	mkdir -p vim/vundle
-	cd vim/vundle
-	git init
-	git remote add origin https://github.com/gmarik/Vundle.vim.git
-	git pull origin master
+# TODO: maybe move vundle installation to ${DOTFILES}/vim/vundle ?
+VUNDLEDIR=${HOME}/.vim/bundle/Vundle.vim
+if [ ! -e "${VUNDLEDIR}" ]; then
+    mkdir -p ${VUNDLEDIR}
+    cd ${VUNDLEDIR}
+    git init
+    git remote add origin https://github.com/gmarik/Vundle.vim.git
+    git pull origin master
 else
-	cd vim/vundle
-	git pull origin master
+    cd ${VUNDLEDIR}
+    git pull origin master
 fi
