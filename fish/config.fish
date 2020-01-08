ab71b5f834c437de4a7894988b411248848a7b90
Cosmin Banu <cosminb@gmail.com>
2020-01-08 13:01:20 +0200

    Add fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,28 @@
+# prompt
+function fish_prompt
+        set_color brblack
+        echo -n "["(date "+%H:%M")"] "
+        set_color yellow
+        echo -n (whoami)
+        set_color normal
+        echo -n "@"
+        set_color blue
+        echo -n (hostname)
+        if [ $PWD != $HOME ]
+                set_color brblack
+                echo -n ':'
+                set_color yellow
+                echo -n (basename $PWD)
+        end
+        set_color green
+        printf '%s ' (__fish_git_prompt)
+        set_color red
+        echo -n '| '
+        set_color normal
+end
+
+# abbreviations
+
 abbr -a l   "exa"
 abbr -a ls  "exa"
 abbr -a ll  "exa -l"
